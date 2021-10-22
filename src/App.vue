d0283475341c8cf8d153743562027b6e050fc291
Christophe Manin <christophe.manin@rcf.fr>
2021-10-22 20:53:53 +0200

    #00003 add Header component

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,6 @@
 <template>
   <div id="app">
-    <div class="container">
-      <h1>Bienvenue sur Name Generator !</h1>
-    </div>
+    <Header />
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
@@ -11,6 +9,36 @@
   </div>
 </template>
 
-<style>
+<script>
+import Header from "./components/Header.vue"
 
+export default {
+  name: "App",
+  components: {
+    Header
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <b-navbar toggleable type="dark" variant="dark">
+      <b-navbar-brand href="#">Name generator</b-navbar-brand>
+
+      <b-navbar-toggle target="navbar-toggle-collapse">
+        <template #default="{ expanded }">
+          <b-icon v-if="expanded" icon="chevron-bar-up"></b-icon>
+          <b-icon v-else icon="chevron-bar-down"></b-icon>
+        </template>
+      </b-navbar-toggle>
+
+      <b-collapse id="navbar-toggle-collapse" is-nav>
+        <b-navbar-nav class="ml-1">
+          <b-nav-item href="#">Link 1</b-nav-item>
+          <b-nav-item href="#">Link 2</b-nav-item>
+          <b-nav-item href="#" disabled>Disabled</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header"
+};
+</script>
+
+<style lang="scss" scoped>
+  .navbar-brand {
+    padding-left: 1%;
+  }
+
+  .navbar-toggler {
+    margin-right: 1%;
+  }
+</style>
