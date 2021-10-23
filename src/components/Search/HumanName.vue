dc42d84a5c5233807ce1e3b7c6d72e7708f3fdd6
Christophe Manin <christophe.manin@rcf.fr>
2021-10-23 20:36:55 +0200

    #00004 define global search components style

--- src/components/Search/HumanName.vue
+++ src/components/Search/HumanName.vue
@@ -3,27 +3,37 @@
     <div class="component-name">
       <h2>Nom humain</h2>
     </div>
-    <div class="gender-container">
-      <b-button variant="light">Masculin</b-button>
-      <b-button variant="light">Féminin</b-button>
-    </div>
-    <div class="search-container">
-      <b-input-group class="firstname-search">
-        <template #prepend>
-          <b-button variant="light">Prénom</b-button>
-        </template>
-        <b-form-input></b-form-input>
-      </b-input-group>
-      <b-input-group class="lastname-search">
-        <template #prepend>
-          <b-button variant="light">Nom</b-button>
-        </template>
-        <b-form-input></b-form-input>
-      </b-input-group>
-    </div>
-    <div class="result-container">
-      <b-form-input></b-form-input>
-    </div>
+
+    <b-container class="gender-container">
+      <b-row align-h="around">
+        <b-button class="gender-button" variant="light">Masculin</b-button>
+        <b-button class="gender-button" variant="light">Féminin</b-button>
+      </b-row>
+    </b-container>
+
+    <b-container class="search-container">
+      <b-row align-h="around">
+        <b-input-group class="firstname-search">
+          <template #prepend>
+            <b-button variant="light">Prénom</b-button>
+          </template>
+          <b-form-input></b-form-input>
+        </b-input-group>
+
+        <b-input-group class="lastname-search">
+          <template #prepend>
+            <b-button variant="light">Nom</b-button>
+          </template>
+          <b-form-input></b-form-input>
+        </b-input-group>
+      </b-row>
+    </b-container>
+
+    <b-container class="result-container">
+      <b-row align-h="center">
+        <b-form-input placeholder='Résultat'></b-form-input>
+      </b-row>
+    </b-container>
     <div class="copy-container">
       <b-button variant="light">Copier</b-button>
     </div>
@@ -37,4 +47,5 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import "../../assets/styles/components/search/global-search.scss";
 </style>
--- src/views/Generator.vue
+++ src/views/Generator.vue
@@ -1,16 +1,22 @@
 <template>
   <div class="generator">
-    <HumanName />
+    <b-container-fluid>
+      <b-row align-h="around">
+        <b-col sm="5">
+          <HumanName />
+        </b-col>
+      </b-row>
+    </b-container-fluid>
   </div>
 </template>
 
 <script>
-import HumanName from "../components/Search/HumanName.vue"
+import HumanName from "../components/Search/HumanName.vue";
 
-  export default {
-    name: 'Generator',
-    components: {
-      HumanName,
-    }
-  }
+export default {
+  name: "Generator",
+  components: {
+    HumanName,
+  },
+};
 </script>
