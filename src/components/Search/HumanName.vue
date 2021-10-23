2e145feee471ff9792ce67307551ae5673232906
Christophe Manin <christophe.manin@rcf.fr>
2021-10-23 18:03:18 +0200

    #00004 Initiate HumanName component

---
+++ src/components/Search/HumanName.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="main-container">
+    <div class="component-name">
+      <h2>Nom humain</h2>
+    </div>
+    <div class="gender-container">
+      <b-button variant="light">Masculin</b-button>
+      <b-button variant="light">Féminin</b-button>
+    </div>
+    <div class="search-container">
+      <b-input-group class="firstname-search">
+        <template #prepend>
+          <b-button variant="light">Prénom</b-button>
+        </template>
+        <b-form-input></b-form-input>
+      </b-input-group>
+      <b-input-group class="lastname-search">
+        <template #prepend>
+          <b-button variant="light">Nom</b-button>
+        </template>
+        <b-form-input></b-form-input>
+      </b-input-group>
+    </div>
+    <div class="result-container">
+      <b-form-input></b-form-input>
+    </div>
+    <div class="copy-container">
+      <b-button variant="light">Copier</b-button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HumanName",
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/views/Generator.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="generator">
+    <HumanName />
+  </div>
+</template>
+
+<script>
+import HumanName from "../components/Search/HumanName.vue"
+
+  export default {
+    name: 'Generator',
+    components: {
+      HumanName,
+    }
+  }
+</script>
